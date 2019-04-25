f51e93e152106d38ba5238e707322a95af99f383
brwaang55 <bruce.waang55@gmail.com>
2019-04-25 16:33:18 -0400

    updating figure code

--- SINATRA_Pipeline_Branch/shape_reconstruction.R
+++ SINATRA_Pipeline_Branch/shape_reconstruction.R
@@ -19,6 +19,29 @@ compute_selected_vertices_cones = function(dir, complex, rate_vals, len, thresho
   total_selected_vertices=Reduce(union,coned_vertices)
   return(total_selected_vertices)
 }
+compute_selected_faces_cones = function(dir, complex, rate_vals, len, threshold=-1, cone_size, ball = TRUE, ball_radius,radius = 0){
+  if (threshold==-1){
+    threshold=1/length(rate_vals)
+  }
+  if ((dim(dir)[1] %% cone_size) != 0){
+    print('Number of Cones not a multiple of directions')
+    return(0)
+  }
+  coned_vertices=list()
+  for (j in 1:(dim(dir)[1] / cone_size)){
+    cone_dirs=matrix(dir[((j-1)*(cone_size)+1):(j*cone_size),],ncol = 3)
+    cone_rate_vals=rate_vals[(j-1)*(cone_size*len)+1:(j*cone_size*len)]
+    coned_vertices[[j]]=summarize_vertices(dir = cone_dirs, complex, rate_vals = cone_rate_vals, len,
+                                           reduction_operation = intersect, threshold, cone_size, ball = ball, ball_radius, radius = radius)
+    if (TRUE %in% (c(2677, 1391, 2891, 1562) %in% coned_vertices[[j]])){
+      #print(j)
+    }
+  }
+  total_selected_vertices=Reduce(union,coned_vertices)
+  reconstructed_faces = apply(X = complex$Faces,MARGIN = 1,function(x) any(x %in% total_selected_vertices))
+  reconstructed_faces = which(reconstructed_faces == TRUE)
+  return(reconstructed_faces)
+}
 
 summarize_vertices=function(dir,complex,rate_vals,len,reduction_operation=intersect,threshold,cone_size, ball = TRUE, ball_radius = 1, radius = 0){
   picked_indices=which(rate_vals>=threshold)
